12bdcf6a598889286b27dd2f931ec2e1874119d2
oicqq <140582646+oicqq@users.noreply.github.com>
2024-06-01 09:57:11 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 REM Display prompt message
 echo 建议在创建python虚拟环境后安装运行。
 echo 可以用conda或以下代码（windows下）：
@@ -10,8 +11,6 @@ REM Step 1: Clone the ChatTTS repository
 echo Cloning ChatTTS repository...
 git clone https://github.com/oicqq/ChatTTS
 
-cd ChatTTS
-
 REM Step 2: Download the model files
 echo Downloading model files...
 git clone https://www.modelscope.cn/pzc163/chatTTS.git
@@ -19,6 +18,7 @@ git clone https://www.modelscope.cn/pzc163/chatTTS.git
 REM Step 3: Install the required Python packages
 echo Installing required Python packages...
 pip install -r re.txt
+
 
 echo 安装完成！
 echo 运行run.bat或在虚拟环境中运行命令 streamlit run app.py
